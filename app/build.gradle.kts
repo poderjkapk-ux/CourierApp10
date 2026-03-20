0c90888061af37cde444ab3540a38e99913cabad
Denis <poderjkapk@gmail.com>
2026-03-20 22:43:54 +0200

    3.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         // ИСПРАВЛЕНИЕ: Рекомендуется также обновить targetSdk до 36
         targetSdk = 35
 
-        versionCode = 23
-        versionName = "1.5.8"
+        versionCode = 24
+        versionName = "1.5.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
